4716f1c101efa99f9c519aec97a8c619665166cc
Imtiaz-U <i.uddin270209@gmail.com>
2026-06-30 13:58:53 +0100

    frontend update

--- app.py
+++ app.py
@@ -1,15 +1,7 @@
 import streamlit as st
-from backend.py import main()
+from (python script.py) import main()
 
 st.title("Wanderwise")
 st.header("your personal travel assistant")
 
-destination = st.text_input("Destination")
-budget = st.number_input("Budget (£)", min_value=0)
-interests = st.text_input("Interests")
-
-if st.button("Generate Itinerary"):
-    itinerary = main(dates, budget ,interests, travel_style)
-
-
-    st.write(itinerary)
+main()
